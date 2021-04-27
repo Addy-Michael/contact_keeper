1d2131bb3c43c03a0d31bfb05895644f509878af
addy michael <addymichael18@outlook.com>
2021-04-27 03:17:25 +0100

    logout implemented

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from "react";
 import axios from "axios";
 import AuthContext from "./authContext";
 import AuthReducer from "./authReducer";
+import setAuthToken from "../../utils/setAuthToken";
 import {
   REMOVE_ALERT,
   REGISTER_SUCCESS,
@@ -27,7 +28,20 @@ const AuthState = (props) => {
   const [state, dispatch] = useReducer(AuthReducer, initialState);
 
   // Load User
-  const loadUser = () => console.log("load user");
+  const loadUser = async () => {
+    // load token into global headers
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+
+    try {
+      const res = await axios.get("/api/auth");
+
+      dispatch({ type: USER_LOADED, payload: res.data });
+    } catch (err) {
+      dispatch({ type: AUTH_ERROR });
+    }
+  };
 
   // Register User
   const register = async (formData) => {
@@ -41,6 +55,8 @@ const AuthState = (props) => {
       const res = await axios.post("/api/users", formData, config);
 
       dispatch({ type: REGISTER_SUCCESS, payload: res.data });
+
+      loadUser();
     } catch (err) {
       dispatch({
         type: REGISTER_FAIL,
@@ -50,10 +66,29 @@ const AuthState = (props) => {
   };
 
   // Login User
-  const login = () => console.log("login user");
+  const login = async (formData) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.post("/api/auth", formData, config);
+
+      dispatch({ type: LOGIN_SUCCESS, payload: res.data });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
 
   // Logout
-  const logout = () => console.log("logout");
+  const logout = () => dispatch({ type: LOGOUT });
 
   // Clear Errors
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
